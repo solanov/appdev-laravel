d56a24cbd38198cd6c3fdeff4d143adc494ff2dd
Vinz Solano <bamharu228@gmail.com>
2026-04-18 16:56:58 +0800

    Completed Lesson 2 Part 3

--- application_layout/resources/views/home.blade.php
+++ application_layout/resources/views/home.blade.php
@@ -5,5 +5,34 @@
 <main role="main" class="container">
     <h1 class="mt-5 text-danger">Home</h1>
     Lorem, ipsum dolor sit amet consectetur adipisicing elit. Cum pariatur ratione quaerat vero a, ullam reiciendis earum distinctio nihil exercitationem quidem neque odit aliquid quasi esse, repudiandae, adipisci non placeat.
+
+    {{-- Using foreach loop --}}
+
+    {{-- <div class="row mt-5">
+        @foreach ($blogs as $blog)
+        <div class="col-md-4">
+            <div class="card">
+                <div class="card-body">
+                    <h2>{{ $blog['title'] }}</h2>
+                    <p>{{ $blog['body'] }}</p>
+                </div>
+            </div>
+        </div>
+        @endforeach
+    </div>--}}
+
+    {{-- Using for loop --}}
+    <div class="row mt-5">
+        @for ($i = 0; $i < count($blogs); $i++)
+        <div class="col-md-4">
+            <div class="card">
+                <div class="card-body">
+                    <h2>{{ $blogs[$i]['title'] }}</h2>
+                    <p>{{ $blogs[$i]['body'] }}</p>
+                </div>
+            </div>
+        </div>
+        @endfor
+    </div>
 </main>
 @endsection
--- application_layout/routes/web.php
+++ application_layout/routes/web.php
@@ -3,7 +3,25 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('home');
+    $blogs=[
+        [
+            'title' => 'This is the title One',
+            'body' => 'This is a body text One'
+        ],
+        [
+            'title' => 'This is the title Two',
+            'body' => 'This is a body text Two'
+        ],
+        [
+            'title' => 'This is the title Three',
+            'body' => 'This is a body text Three'
+        ],
+        [
+            'title' => 'This is the title Four',
+            'body' => 'This is a body text Four'
+        ],
+    ];
+    return view('home', compact('blogs'));
 });
 
 Route::get('/about', function () {
