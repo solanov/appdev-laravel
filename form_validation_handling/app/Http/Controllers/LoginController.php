08eae051d22e63fd9397f8780a263fa2585da905
Vinz Solano <bamharu228@gmail.com>
2026-04-28 22:59:43 +0800

    Lesson 4: Validation Handling Overwrite Error Message

--- form_validation_handling/app/Http/Controllers/LoginController.php
+++ form_validation_handling/app/Http/Controllers/LoginController.php
@@ -24,6 +24,16 @@ class LoginController extends Controller
                 'name' => ['required','alpha'],
                 'email' => ['required','email'],
                 'password' => ['required','min:6','max:10']
+            ],
+            // Changing validation default message
+            [
+                'name.required' => 'Attention: Please Fill up the Username field to continue!',
+                'name.alpha' => 'Attention: User name should contain letter only!',
+                'email.required' => 'Attention: Please Fill up the Email field to continue!',
+                'email.email' => 'Attention: Please use a valid email address to continue!',
+                'password.required' => 'Attention: Please Fill up the Password field to continue!',
+                'password.min' => 'Attention: Password must be at least 6 characters',
+                'password.max' => 'Attention: Password must not exceed 10 characters'
             ]
         );
         return $request;
