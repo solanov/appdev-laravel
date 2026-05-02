80c07291005d72b10839d5e78de9ecee6d46c3e0
Vinz Solano <bamharu228@gmail.com>
2026-05-02 21:22:56 +0800

    Lesson 5: Environment Initialised

--- db_query_builder/app/Http/Controllers/QueryBuilderContoller.php
+++ db_query_builder/app/Http/Controllers/QueryBuilderContoller.php
@@ -6,5 +6,7 @@ use Illuminate\Http\Request;
 
 class QueryBuilderContoller extends Controller
 {
-    //
+    public function index(){
+        return '<h1>Hello</h1>';
+    }
 }
--- db_query_builder/routes/web.php
+++ db_query_builder/routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
+use App\Http\Controllers\QueryBuilderContoller;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/querybuilder', [QueryBuilderContoller::class, 'index']);
+
