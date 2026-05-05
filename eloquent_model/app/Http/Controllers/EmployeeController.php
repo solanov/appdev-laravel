cb21e5245ac9791700704fd77a59c7b081cdaa6d
Vinz Solano <bamharu228@gmail.com>
2026-05-05 18:26:25 +0800

    Lesson 6: Retieving Records Via Eloquent Models

--- eloquent_model/app/Http/Controllers/EmployeeController.php
+++ eloquent_model/app/Http/Controllers/EmployeeController.php
@@ -12,6 +12,18 @@ class EmployeeController extends Controller
      */
     public function __invoke(Request $request)
     {
-        return $employees = Employee::all();
+        //return $employees = Employee::all();
+        //return Employee::find(4);
+        //$employee = Employee::find(4);
+        //return $employee->first_name.' '.$employee->last_name;
+
+        //return $employee=Employee::find(52);
+        //return $employee=Employee::findOrFail(52);
+
+        $employees = Employee::all();
+
+        foreach($employees as $employee){
+            echo $employee->first_name.' '.$employee->last_name.'<br>';
+        }
     }
 }
